3a2df1cb27e3115c0d6f75c78e4daa6fb17ef3cc
YongliuLi <ylli@thoughtworks.com>
2016-08-14 18:46:03 +0800

    [#0] service implement

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,11 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
-
-export class Hero {
-  id: number;
-  name: string;
-}
 
 @Component({
+	providers: [HeroService],
   selector: 'my-app',
   styles: [`
 	  .selected {
@@ -74,33 +60,31 @@ template:`
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
 	<ul class="heroes">
-	  <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+	  <li *ngFor="let hero of heroes" 
+	  [class.selected]="hero === selectedHero"
+	  (click)="onSelect(hero)">
 	    <span class="badge">{{hero.id}}</span> {{hero.name}}
 	  </li>
 	</ul>
-  <div *ngIf="selectedHero">
-	  <h2>{{selectedHero.name}} details!</h2>
-		<div><label>id: </label>{{selectedHero.id}}</div>
-		<div>
-		    <label>name: </label>
-		    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-		</div>
-	</div>
-
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `
 
 })
 
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+	constructor(private heroService: HeroService) {
+		
+	}
+	ngOnInit() {
+    this.getHeroes();
+  }
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => { this.heroes = heroes});
+  }
 	title = 'Tour of Heroes';
-	public heroes = HEROES;
+	heroes: Hero[];
 	selectedHero: Hero;
-	hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   onSelect(hero: Hero) { this.selectedHero = hero; }
-
 
 }
