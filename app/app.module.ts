e69a9f788a5f1809ea550cd815e5276d89c60a9c
YongliuLi <ylli@thoughtworks.com>
2016-08-14 19:05:20 +0800

    [#0] create heros component

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
 import { HeroDetailComponent } from './hero-detail.component';
-import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component'
+import { AppComponent }  from './app.component'
+import { HeroService }  from './hero.service';
+
 
 @NgModule({
   imports:      [ BrowserModule ,
   				 FormsModule],
   declarations: [ 
   	AppComponent,
-  	HeroDetailComponent
+  	HeroDetailComponent,
+  	HeroesComponent
   ],
+
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/app.routing.ts
@@ -0,0 +1,10 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HeroesComponent } from './heroes.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+];
